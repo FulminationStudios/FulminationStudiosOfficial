192f2955d761a6b0046d3407803c02824676275b
FulminationStudios <fulminstudios@gmail.com>
2016-05-31 00:19:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,19 @@ var main = function() {
   $('.Tab').first().addClass('ActiveTab');
   $('.Tab').click(function () {
     $(this).addClass('ActiveTab');
-    $(this).siblings().removeClass('ActiveTab')
+    $(this).siblings().removeClass('ActiveTab');
+    hidePages();
+    if ($(this).hasClass('HomeTab')) {
+      $('.HomePage').show();
+    } else if ($(this).hasClass('NewsTab')) {
+      $('.NewsPage').show();
+    } else if ($(this).hasClass('ProjectsTab')) {
+      $('.ProjectsPage').show();
+    } else if ($(this).hasClass('AboutTab')) {
+      $('.AboutPage').show();
+    } else if ($(this).hasClass('BlogTab')) {
+      $('.BlogPage').show();
+    }
   });
 };
 var hidePages = function() {
