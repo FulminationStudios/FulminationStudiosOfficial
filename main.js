0c2c0e4a1b67dfe1bd1f9e8495d3fc93bc967e12
FulminationStudios <fulminstudios@gmail.com>
2016-05-30 23:49:40 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var main = function() {
   
   $('.Tab').click(function () {
     $(this).addClass('ActiveTab');
-    $(this).siblings().removeClass('ActiveTab').addClass('Tab');
+    $(this).parent().children().removeClass('ActiveTab').addClass('Tab');
   });
 };
 $(document).ready(main);
