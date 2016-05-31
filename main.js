73028f0cae82a86e118d675a0c194716ae520298
FulminationStudios <fulminstudios@gmail.com>
2016-05-31 00:06:03 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var main = function() {
+  $('.HomePage').show();
+  $('.NewsPage').hide();
+  $('.ProjectsPage').hide();
+  $('.AboutPage').hide();
+  $('.BlogPage').hide();
   $('.Tab').first().addClass('ActiveTab');
   $('.Tab').click(function () {
     $(this).addClass('ActiveTab');
