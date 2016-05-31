67c9c603d64b3be6ae034447d700edd6bf860cad
FulminationStudios <fulminstudios@gmail.com>
2016-05-31 00:12:37 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 var main = function() {
-  $('.HomePage').show();
-  $('.NewsPage').hide();
-  $('.ProjectsPage').hide();
-  $('.AboutPage').hide();
-  $('.BlogPage').hide();
+  hidePages();
   $('.Tab').first().addClass('ActiveTab');
   $('.Tab').click(function () {
     $(this).addClass('ActiveTab');
     $(this).siblings().removeClass('ActiveTab')
+    if ($(this).hasClass('HomePage'))
   });
 };
+var hidePages = function() {
+  $('.HomePage').hide();
+  $('.NewsPage').hide();
+  $('.ProjectsPage').hide();
+  $('.AboutPage').hide();
+  $('.BlogPage').hide();
+}
 $(document).ready(main);
